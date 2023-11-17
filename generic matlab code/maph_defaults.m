a480d7940879174f2466f89a030f25f7f8bc5db5
Madison Phelps <104942916+phelpmad@users.noreply.github.com>
2023-11-16 16:58:17 -0800

    Add files via upload

---
+++ generic matlab code/maph_defaults.m
@@ -0,0 +1,11 @@
+%set(groot,'defaultLineLineWidth',4)
+%set(0,'DefaultLineMarkerSize',10);
+%set(0,'DefaultaxesLineWidth', 3)
+%set(0,'DefaultaxesFontSize', 24)
+%set(0,'DefaultTextFontSize', 18)
+
+set(groot,'defaultLineLineWidth',8)
+set(0,'DefaultaxesLineWidth', 6)
+set(0,'DefaultaxesFontSize', 36)
+set(0,'DefaultTextFontSize', 30)
+
